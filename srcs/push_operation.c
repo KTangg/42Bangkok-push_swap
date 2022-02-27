2a2bee4fd06ff3848131657beb3da7551bef0d24
KTangg <k.tangtangtang@gmail.com>
2022-02-27 17:55:26 +0700

    Initial commit

---
+++ srcs/push_operation.c
@@ -0,0 +1,100 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   put_operation.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: spoolpra <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/27 17:28:42 by spoolpra          #+#    #+#             */
+/*   Updated: 2022/02/27 17:28:42 by spoolpra         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include "push_swap.h"
+
+static int	*intcpy(int *dst, int *src, size_t size)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < size)
+	{
+		dst[i] = src[i];
+		i++;
+	}
+	return (dst);
+}
+
+static int	*cut_top(int *array, size_t size)
+{
+	int	*new_array;
+
+	if (size == 1)
+	{
+		free(array);
+		return(NULL);
+	}
+	new_array = (int *)malloc(sizeof(int) * (size - 1));
+	if (!new_array)
+	{
+		free(array);
+		return (NULL);
+	}
+	free(array);
+	new_array = intcpy(new_array, array + 1, size - 1);
+	return (new_array);
+}
+
+static int	*add_top(int *array, size_t size, int new)
+{
+	int	*new_array;
+
+	new_array = (int *)malloc(sizeof(int) * (size + 1));
+	if (!new_array)
+	{
+		if (array)
+			free(array);
+		return (NULL);
+	}
+	new_array[0] = new;
+	if (size > 0)
+		intcpy(&new_array[1], array, size);
+	if (array)
+		free(array);
+	return (new_array);
+}
+
+int	push_a(t_stack *a, t_stack *b)
+{
+	int	tmp;
+
+	tmp = a->i_array[0];
+	a->i_array = cut_top(a->i_array, a->size);
+	a->size = a->size - 1;
+	if (a->size != 0 && tmp == NULL)
+		return (0);
+	b->i_array = add_top(b->i_array, b->size, tmp);
+	b->size = b->size + 1;
+	if (!b->i_array)
+		return (0);
+	ft_printf("pa\n");
+	return (1);
+}
+
+int	push_b(t_stack *a, t_stack *b)
+{
+	int	tmp;
+
+	tmp = b->i_array[0];
+	b->i_array = cut_top(b->i_array, b->size);
+	b->size = b->size - 1;
+	if (b->size != 0 && tmp == NULL)
+		return (0);
+	a->i_array = add_top(a->i_array, a->size, tmp);
+	a->size = a->size + 1;
+	if (!a->i_array)
+		return (0);
+	ft_printf("pb\n");
+	return (1);
+}
---
+++ srcs/swap_operation.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   swap_operation_1.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: spoolpra <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/27 17:21:28 by spoolpra          #+#    #+#             */
+/*   Updated: 2022/02/27 17:21:28 by spoolpra         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include "push_swap.h"
+
+static void	top_swap(t_stack a)
+{
+	int	tmp;
+
+	tmp = a.i_array[0];
+	a.i_array[0] = a.i_array[1];
+	a.i_array[1] = tmp;
+}
+
+void	swap_a(t_stack a)
+{
+	top_swap(a);
+	ft_printf("sa\n");
+}
+
+void	swap_b(t_stack b)
+{
+	top_swap(b);
+	ft_printf("sa\n");
+}
+
+void	push_swap_ss(t_stack a, t_stack b)
+{
+	top_swap(a);
+	top_swap(b);
+	ft_printf("ss\n");
+}
