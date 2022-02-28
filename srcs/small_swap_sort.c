a7cd1d800267f6fbc498851037b969d4238de256
KTangg <k.tangtangtang@gmail.com>
2022-02-28 15:13:41 +0700

    update sort_3 conditions

--- srcs/small_swap_sort.c
+++ srcs/small_swap_sort.c
@@ -31,7 +31,7 @@ static void	sort_stack_3(t_stack **a)
 	}
 	else if (top > mid && mid < bot && bot < top)
 		rotate_a(a);
-	else if (top < mid && mid > bot && bot < top)
+	else if (top < mid && mid > bot && bot > top)
 	{
 		swap_a(a);
 		rotate_a(a);
