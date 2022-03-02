e870bfbd14a18a2bfb02d161cba7480ef99a2185
KTangg <k.tangtangtang@gmail.com>
2022-03-02 20:43:51 +0700

    fix norm

--- srcs/rotate_minmax.c
+++ srcs/rotate_minmax.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   rotate_minmax.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: spoolpra <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: spoolpra <spoolpra@student.42bangkok.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/01 13:36:04 by spoolpra          #+#    #+#             */
-/*   Updated: 2022/03/01 13:36:04 by spoolpra         ###   ########.fr       */
+/*   Updated: 2022/03/02 20:43:37 by spoolpra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,7 @@ size_t	index_finder(t_stack **stack, int i)
 	return (0);
 }
 
-int	dir_check(t_stack **stack, int	to)
+int	dir_check(t_stack **stack, int to)
 {
 	int	all;
 	int	to_index;
@@ -66,41 +66,3 @@ void	rotate_to(t_stack **stack, int to, char c)
 		}
 	}
 }
-
-/*void	rotate_to_min(t_stack **stack, int min, char c)
-{
-	t_stack	*last;
-
-	last = stack_last(*stack);
-	if (c == 'a')
-	{
-		while ((*stack)->i != min)
-			reverse_rotate_a(stack);
-	}
-	if (c == 'b')
-	{
-		if (last->i == min && (*stack)->i != last->i)
-			reverse_rotate_b(stack);
-		while ((*stack)->i != min)
-			rotate_b(stack);
-	}
-}
-
-void	rotate_to_max(t_stack **stack, int max, char c)
-{
-	t_stack	*last;
-
-	last = stack_last(*stack);
-	if (c == 'a')
-	{
-		if (last->i == max && (*stack)->i != last->i)
-			reverse_rotate_a(stack);
-		while ((*stack)->i != max)
-			rotate_a(stack);
-	}
-	if (c == 'b')
-	{
-		while ((*stack)->i != max)
-			reverse_rotate_b(stack);
-	}
-}*/
